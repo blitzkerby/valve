6bd03d75532dd343eba565ddd02029d9e71c2d40
K3RBY <hakkerby01@gmail.com>
2024-07-25 16:41:42 +0700

    Auto commit

---
+++ .react_starter/components/profile-card/profile_card.jsx
@@ -0,0 +1,4 @@
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import "./profile_card.css"
+
---
+++ .react_starter/constants/skills.js
@@ -0,0 +1,20 @@
+const skills = [
+    {   skills: "HTML-CSS"  ,
+        level : "advanced"  ,
+        color : "#2662EA"   , 
+    }, 
+    {   skill : "JavaScript", 
+        level : "intermediate",
+        color : "#F74B4B"   , 
+    },
+    {   skills: "Web Desing",
+        level : "advanced"  ,
+        color : "#00B3E6"   ,
+    },
+    {   skills: "Git and GitHub",
+        level : "advanced"  ,
+        color : "#333333"   ,
+    }
+    // Add more skills as needed
+]
+

d51288de1672c90b4d771879db7d96f33a25b1e0
Admin-Kerby <hakkerby01@gmail.com>
2024-07-30 10:40:11 +0700

    saving notes for today

--- .project/src/proto/far-away/components/Form.jsx
+++ .project/src/proto/far-away/components/Form.jsx
@@ -1,19 +1,23 @@
 import { useState } from "react"
 
 export default function Form() {
-    const [description, setDescription] = useState("Test")
+    const [description  , setDescription]   = useState("Test")
+    const [quantity     , setQuantity]      = useState(5)
+    
 
 
     function handleSubmit(e) {
         e.preventDefault();
-        // console.log(e)
+
+        const newItem = {description, quantity, packed:false, id: Date.now() };
+        console.log(newItem)
     }
 
     return (
         <form className="add-form" onSubmit={handleSubmit}>
             <h3>What do you need for your trip? ✨</h3>
 
-            <select>
+            <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
                 { Array.from({length:20}, (_, i) => i + 1).map(
                     (num) => (
                         <option value={num} key={num}>{num}</option>
--- .project/src/proto/far-away/components/Stats.jsx
+++ .project/src/proto/far-away/components/Stats.jsx
@@ -1,5 +1,5 @@
 export default function Stats() {
-    return <footer>
+    return <footer className="stats">
         <em>🧳 You have X items on your list, and you already packed X (X%).</em>
     </footer>
 }

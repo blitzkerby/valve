64b104cf056f26b943304da6f64ba7a0ca231f04
K3RBY <hakkerby01@gmail.com>
2024-08-01 23:12:20 +0700

    Auto commit

--- .project/src/proto/far-away/components/PackingList.jsx
+++ .project/src/proto/far-away/components/PackingList.jsx
@@ -1,40 +1,49 @@
-const initialItems = [
-    {id:1,
-        description: "Passports",
-        quantity: 2,
-        packed: true
-    },
-    {id:2,
-        description: "Travel documents",
-        quantity: 1,
-        packed: false
-    },
-    {id:3,
-        description: "Credit cards",
-        quantity: 3,
-        packed: false
-    }
-]
+import { useState } from "react";
+import Item from "./Item";
 
-function Item({item}){
-    return <li key={item.id}>
-        <input type="checkbox" value={item.packed} readOnly />
-        <span style={item.packed? {textDecoration: "line-through"} : {}}>
-            {item.quantity} {item.description}
-        </span>
-        <button>❌</button>
-    </li>
-}
+export default function PackingList({
+  items,
+  onDeleteItem,
+  onToggleItem,
+  onClearList,
+}) {
+  const [sortBy, setSortBy] = useState("input");
 
+  let sortedItems;
 
-export default function PackingList({items , onDeleteItem}) {
-    return (
-        <div className="list">
-            <ul>
-                { items.map(item => {
-                        <Item item={item}/>
-                    })}
-            </ul>
-        </div>
-    )
+  if (sortBy === "input") sortedItems = items;
+
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+
+  return (
+    <div className="list">
+      <ul>
+        {sortedItems.map((item) => (
+          <Item
+            item={item}
+            onDeleteItem={onDeleteItem}
+            onToggleItem={onToggleItem}
+            key={item.id}
+          />
+        ))}
+      </ul>
+
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+        <button onClick={onClearList}>Clear list</button>
+      </div>
+    </div>
+  );
 }

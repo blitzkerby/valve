ecde9f6a11df3d95bb0498142235bb037e5894f0
K3RBY <hakkerby01@gmail.com>
2024-08-03 05:09:06 +0700

    Auto commit

--- .project/src/proto/using-apis/api.js
+++ .project/src/proto/using-apis/api.js
@@ -1,18 +1,1 @@
-import axios from 'axios';
-
-const searchImages = async () => {
-  const response = await axios.get('https://api.unsplash.com/search/photos', {
-    headers: {
-      Authorization: 'Client-ID 8O50V7bNzfKdVixwS9W9nZVdr0VnrCv9gmeimfdvp6Y',
-    },
-    params: {
-      query: 'cars',
-    },
-  });
-
-  console.log(response);
-
-  return response;
-};
-
-export default searchImages;
+import axios from "axios"

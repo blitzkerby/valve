0b1026cb844afd890a1937089c5582d6a6e4b9fe
K3RBY <hakkerby01@gmail.com>
2024-08-06 10:04:20 +0700

    Auto commit

--- .project/vite/src/proto/07 - text-expander/main.jsx
+++ .project/vite/src/proto/07 - text-expander/main.jsx
@@ -1,10 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom"
+
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
 
 import App from "./components/App";
 
 const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
+const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <StrictMode>
